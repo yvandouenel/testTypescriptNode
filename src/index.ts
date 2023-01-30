e5bb88445bb240a469abfc66cd01db56714a618d
yvandouenel <y.douenel@coopernet.fr>
2023-01-30 14:54:01 +0100

    ajout du répertoire models

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,7 @@
 import { getUser } from './services/user';
-console.log('Hello world!')
-
-
-
-export interface UserInterface {
-    id: number;
-    name: string;
-}
-const account = async() => {
+(async () => {
     const user = await getUser();
     console.log(`user : `, user);
-}
-account();
+})();
 
 
--- src/services/user.ts
+++ src/services/user.ts
@@ -1,5 +1,5 @@
 import user from "../mocks/user.json"
-import { UserInterface } from "../index";
+import { UserInterface } from "../models/User";
 
 export function getUser(): Promise<UserInterface> {
     return new Promise((resolve => resolve(user)))
